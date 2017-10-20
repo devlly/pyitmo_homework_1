b04d3866387aa7f2b6183a0f98d014c106101c50
Polina Popova <devlly@mail.ru>
2017-10-20 20:52:12 +0000

    hm9

--- organazer/organazer_code/storage.py
+++ organazer/organazer_code/storage.py
@@ -1,69 +1,7 @@
 import sqlite3
 import datetime
+import os.path as Path
 
-'''SQL_SELECT_ALL_REGTASKS = """
-    SELECT id, regtasks_name, regtask_content, regtask_status, regtasks_weekday, regtasks_time
-    FROM regtasks
-""" '''
-
-SQL_SELECT_ALL_ONETIME_TASKS = """
-    SELECT id,
-    onetime_tasks_name,
-    onetime_tasks_content,
-    onetime_tasks_day,
-    onetime_tasks_time,
-    onetime_tasks_status,
-    FROM onetime_tasks
-"""
-
-'''SQL_SELECT_ALL_CALENDAR = """
-    SELECT id, cal_plan_date, cal_tasks_body
-
-""" '''
-
-SQL_SELECT_ALL_BY_DAY = SQL_SELECT_ALL_ONETIME_TASKS + " WHERE onetime_tasks_day=?"
-SQL_SELECT_BY_NAME = SQL_SELECT_ALL_ONETIME_TASKS + " WHERE onetime_tasks_name=?"
-
-'''SQL_INSERT_REGTASKS = """
-    INSERT INTO regtasks (
-    regtasks_name,
-    regtasks_content,
-    regtasks_weekday,
-    regtime_time
-    )
-     VALUES (?, ?, ?, ?)
-""" '''
-
-SQL_INSERT_ONETIME_TASKS = """
-    INSERT INTO onetime_tasks (
-    onetime_tasks_name,
-    onetime_tasks_content,
-    onetime_tasks_day,
-    onetime_tasks_time
-    )
-    VALUES (?, ?, ?, ?)
-"""
-
-
-SQL_UPDATE_ONETIME_TASK_CONTENT = """
-    UPDATE onetime_tasks SET onetime_tasks_content=? WHERE id=?
-"""
-
-SQL_UPDATE_ONETIME_TASK_NAME = """
-    UPDATE onetime_tasks SET onetime_tasks_name=? WHERE id=?
-"""
-
-SQL_UPDATE_ONETIME_TASK_DAY = """
-    UPDATE onetime_tasks SET onetime_tasks_day=? WHERE id=?
-"""
-
-SQL_UPDATE_ONETIME_TASK_TIME = """
-    UPDATE onetime_tasks SET onetime_tasks_time=? WHERE id=?
-"""
-
-SQL_UPDATE_ONETIME_TASK_STATUS = """
-    UPDATE onetime_tasks SET onetime_tasks_status=? WHERE id=?
-"""
 
 def dict_factory(cursor, row):
     d = {}
@@ -77,116 +15,162 @@ def connect(db_name=None):
         db_name = ':memory:'
 
     conn = sqlite3.connect(db_name)
-    return conn
+    conn.row_factory = dict_factory
 
+    return conn
 
 def initialize(conn, creation_schema):
     with conn, open(creation_schema) as f:
         conn.executescript(f.read())
 
+SQL_SELECT_ALL_TASKS = """
+    SELECT tasks.id_tasks,
+    tasks.tasks_name,
+    tasks.tasks_content,
+    tasks.tasks_status,
+    cal.tasks_datetime
+    FROM tasks, cal
+    WHERE tasks.id_tasks=cal.id_tasks
+    GROUP BY tasks_status
+"""
+SQL_SELECT_ALL_BY_DAY = """
+    SELECT tasks.id_tasks,
+    tasks.tasks_name,
+    tasks.tasks_content,
+    tasks.tasks_status,
+    cal.tasks_date,
+    cal.tasks_time
+    FROM tasks, cal
+    WHERE tasks.id_tasks=cal.id_tasks
+    AND tasks_date=?
+    GROUP BY tasks_date
+"""
+SQL_SELECT_ACTUALL = """
+    SELECT tasks.id_tasks,
+    tasks.tasks_name,
+    tasks.tasks_content,
+    tasks.tasks_status,
+    cal.tasks_date,
+    cal.tasks_time
+    FROM tasks, cal
+    WHERE tasks.id_tasks=cal.id_tasks
+    HAVING tasks.tasks_status='Не выполнено'
+    GROUP BY cal.tasks_date
+"""
 
-def all_tasks(conn):
-    '''Выводит все задачи за все время'''
-    with conn:
-        cursor = conn.execute(SQL_SELECT_ALL_ONETIME_TASKS)
-        return sorted(cursor.fetchall(), key=onetime_tasks_status)
+SQL_SELECT_ID_BY_NAME = """
+    SELECT id_tasks
+    FROM tasks
+    WHERE tasks_name=?
+    """
+SQL_SELECT_BY_NAME = """
+    SELECT tasks.id_tasks,
+    tasks.tasks_name,
+    tasks.tasks_content,
+    tasks.tasks_status,
+    cal.tasks_date,
+    cal.tasks_time
+    FROM tasks, cal
+    WHERE tasks.tasks_name=?
+    GROUP BY cal.tasks_date
+    """
 
-
-def find_task_by_day(conn, onetime_tasks_day):
-    ''''выводит список задач по дню'''
-    with conn:
-        cursor = conn.execute(SQL_SELECT_ALL_BY_DAY, (onetime_tasks_day,))
-        return cursor.fetchall()
-
-
-def task_list_for_today(conn):
-    ''' выводит список задач на сегодня'''
-    with conn:
-        cursor = conn.execute(SQL_SELECT_ALL_BY_DAY, (CURRENT_DATE,))
-        return cursor.fetchall()
-
-
-def find_task_by_name(conn, onetime_tasks_name):
-    '''находит задачу по имени'''
-    onetime_tasks_name = onetime_tasks_name.lower.rsplit('. ', 1).pop()
-    with conn:
-        cursor = conn.execute(SQL_SELECT_BY_NAME,
-                            (onetime_tasks_name,))
-        return cursor.fetchone()
-
-
-def change_onetime_task_name(conn, onetime_tasks_name):
-    '''изменяет имя задачи'''
-    with conn:row = storage.find_task_by_name(conn, name)
-        cursor = conn.execute(SQL_UPDATE_ONETIME_TASK_NAME, (onetime_tasks_name,))
-        return cursor
-
-
-def change_onetime_task_content(conn, onetime_tasks_content):
-    '''изменяет содержание задачи'''
-    with conn:
-        cursor = conn.execute('''
-        SQL_UPDATE_ONETIME_TASK_CONTENT,
-        (onetime_tasks_content,)
-        ''')
-        return cursor
-
-def change_onetime_task_date(conn, onetime_tasks_date):
-    '''изменяет день задачи'''
-    with conn:
-        cursor = conn.execute('''
-        SQL_UPDATE_ONETIME_TASK_DAY,
-        (onetime_tasks_day,)
-        ''')
-        return cursor
-
-def change_onetime_task_time(conn, onetime_tasks_time):
-    '''изменяет время начала задачи'''
-    with conn:
-        cursor = conn.execute('''
-        SQL_UPDATE_ONETIME_TASK_TIME,
-        (onetime_tasks_time,)
-        ''')
-        return cursor
-
-def change_onetime_task_status(conn, onetime_tasks_status):
-    '''изменяет статус задачи'''
-    with conn:
-        cursor = conn.execute('''
-        SQL_UPDATE_ONETIME_TASK_STATUS,
-        (onetime_tasks_status,)
-        ''')
-        return cursor
+SQL_INSERT_TASKS = """
+    INSERT INTO tasks (
+    tasks_name,
+    tasks_content
+    )
+    VALUES (?, ?)
+"""
+SQL_INSERT_CAL = """
+    INSERT INTO cal (
+    tasks_date,
+    tasks_time,
+    id_tasks
+    )
+    VALUES (?, ?, ?)
+"""
 
 
-def add_onetime_tasks(conn, onetime_tasks_name, onetime_tasks_content, onetime_tasks_weekday, onetime_tasks_time):
+
+SQL_UPDATE_TASK_CONTENT = """
+    UPDATE tasks SET tasks_content=? WHERE id_tasks=?
+"""
+
+SQL_UPDATE_TASK_NAME = """
+    UPDATE tasks SET tasks_name=? WHERE id_tasks=?
+"""
+
+SQL_UPDATE_TASK_DATE = """
+    UPDATE tasks SET tasks_day=? WHERE id_tasks=?
+"""
+
+SQL_UPDATE_TASK_TIME = """
+    UPDATE tasks SET tasks_time=? WHERE id_tasks=?
+"""
+
+SQL_UPDATE_TASK_STATUS = """
+    UPDATE tasks SET tasks_status=? WHERE id_tasks=?
+"""
+
+def add_task(conn, tasks_name, tasks_content, tasks_date, tasks_time):
     '''добавляет задачу'''
-    onetime_tasks_name = onetime_tasks_name.lower.rsplit('. ', 1).pop()
+    tasks_name = tasks_name.lower.rsplit('. ', 1).pop()
     with conn:
-        found = find_task_by_name(conn, onetime_tasks_name)
-
+        found = find_task_by_name()
         if found:
-            print('Task already exist. You can change it or start again in change-task nemu')
-            return found.get('''
-        onetime_tasks_name,
-        onetime_tasks_content,
-        onetime_tasks_day,
-        onetime_tasks_time,
-        onetime_tasks_status
-        ''')
+            print('Задача уже существует. Вы можете изменить задачу или начать ее заново')
+        else:
+            cursor = conn.execute(SQL_INSERT_TASKS, (tasks_name, tasks_content))
+            pk = cursor.lastrowid
+            cursor = conn.execute(SQL_INSERT_CAL, (tasks_date, tasks_time, pk))
 
-        cursor = conn.executescript('''
-                             SQL_INSERT_ONETIME_TASKS,
-                             (
-                             onetime_tasks_name,
-                             onetime_tasks_content,
-                             onetime_tasks_day,
-                             onetime_tasks_time,
-                             )
-                             ''')
-        change_onetime_task_name()
-        change_onetime_task_content()
-        change_onetime_task_day()
-        change_onetime_task_time()
+        return tasks_name, tasks_content, tasks_date, tasks_time
 
-        return cursor
+
+def find_task_by_name(conn, tasks_name):
+    '''находит задачу по имени'''
+    tasks_name = onetime_tasks_name.lower.rsplit('. ', 1).pop()
+    with conn:
+        cursor = conn.execute(SQL_SELECT_BY_NAME, (tasks_name,))
+        return cursor.fetchone()
+    #ввести обработку ошибки, если не нашел (алгоритмы поиска)
+
+
+def find_task_by_day(conn, tasks_date):
+    '''выводит список задач по дню'''
+    with conn:
+        cursor = conn.execute(SQL_SELECT_ALL_BY_DAY, (tasks_day,))
+        return cursor.fetchall()
+
+
+def show_acutal_tasks(conn):
+    with get_connection() as conn:
+        cursor = conn.execute(SQL_SELECT_ACTUALL)
+        return cursor.fetchall()
+
+
+def change_task(conn, tasks_name, tasks_content, tasks_date, tasks_time):
+    '''коректирует параметры задачи'''
+    with conn:
+        if tasks_name:
+            cursor = conn.execute(SQL_UPDATE_TASK_NAME, (tasks_name, pk))
+        if tasks_content:
+            cursor = conn.execute(SQL_UPDATE_TASK_CONTENT, (tasks_content, pk))
+        if tasks_date:
+            cursor = conn.execute(SQL_UPDATE_TASK_DATE, (tasks_date, pk))
+        if tasks_time:
+            cursor = conn.execute(SQL_UPDATE_TASK_TIME, (tasks_time, pk))
+    return i
+
+def change_status(conn, tasks_status):
+    with conn:
+        cursor = conn.execute(SQL_UPDATE_TASK_STATUS, (tasks_name, pk))
+    return
+
+
+# def all_tasks(conn):
+#     '''Выводит все задачи за все время'''
+#     with conn:
+#         cursor = conn.execute(SQL_SELECT_ALL_TASKS)
+#         return cursor.fetchall()
